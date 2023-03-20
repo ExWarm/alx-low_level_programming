93be5a91ad81735aed3ff0c3158a57b72bc08fab
Samuel <samuelsawyer0@gmail.com>
2023-03-20 12:43:41 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main function
  * Return: 0
@@ -8,7 +9,7 @@ int main(void)
 {
 	char i;
 
-	for (i = 'z' ; i >= 'a' ; i++)
+	for (i = 'z' ; i >= 'a' ; i--)
 		putchar(i);
 	putchar('\n');
 	return (0);

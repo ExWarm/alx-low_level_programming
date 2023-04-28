354decee760e21a435d09ed421dbf022ddde72cb
ExWarm <samuelsawyer0@gmail.com>
2023-04-28 12:16:31 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+    msg db "Hello, Holberton",0
+    fmt db "%s\n",0
+
+section .text
+    global main
+
+main:
+    mov rdi, fmt
+    mov rsi, msg
+    xor eax, eax
+    call printf
+    xor eax, eax
+    ret
